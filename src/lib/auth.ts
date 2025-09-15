968eae72376c33697dfaee6a846fa124336e3066
sapphire132 <gemg2019@gmail.com>
2025-09-16 02:18:32 +0300

    deployment fix #1

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { expo } from "@better-auth/expo";
 import prisma from "../../prisma/index.js";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  plugins: [expo({ overrideOrigin: true }) as any],
   emailAndPassword: {
     enabled: true,
   },
